3600f940cabb21e87eb496f191ac7bbec7537c79
Darrell Pawson <dpawson905@gmail.com>
2021-08-09 11:27:59 -0400

    update emails

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const Email = require('../utils/email');
+const emailUrl = require('../utils/urls');
 
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
@@ -9,12 +11,17 @@ module.exports.register = async (req, res, next) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
+        const url = emailUrl.setUrl(req, 'auth', `token`);
+        await new Email(registeredUser, url).sendWelcome('Hello');
+        req.flash('success', 'Check your email');
+        return res.redirect('/');
+        // req.login(registeredUser, err => {
+        //     if (err) return next(err);
+        //     req.flash('success', 'Welcome to Yelp Camp!');
+        //     res.redirect('/campgrounds');
+        // })
     } catch (e) {
+        console.log(e.message);
         req.flash('error', e.message);
         res.redirect('register');
     }
--- utils/email.js
+++ utils/email.js
@@ -1,43 +1,49 @@
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const ejs = require('ejs');
-const htmlToText = require('html-to-text');
+const ejs = require("ejs");
+const htmlToText = require("html-to-text");
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
+    this.username = user.username;
     this.url = url;
     this.from = `${process.env.NAME} <${process.env.EMAIL_FROM}>`;
   }
 
   async send(template, subject) {
-    const html = ejs.renderFile(
-      `${__dirname}/../views/emails/${template}.ejs`,
-      {
+    let emailTemplate;
+    ejs
+      .renderFile(`${__dirname}/../views/email/${template}.ejs`, {
+        username: this.username,
         url: this.url,
         subject,
-      }
-    );
-    const mailOptions = {
-      from: this.from,
-      to: this.to,
-      subject,
-      html,
-      text: htmlToText.convert(html),
-    };
-
-    await sgMail.send(mailOptions);
+      })
+      .then(async (result) => {
+        emailTemplate = result;
+        const mailOptions = {
+          from: this.from,
+          to: this.to,
+          subject,
+          html: emailTemplate,
+          text: htmlToText.convert(emailTemplate),
+        };
+        await sgMail.send(mailOptions);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   async sendWelcome() {
-    await this.send('welcome', 'Welcome to the YelpCamp Family');
+    await this.send("welcome", "Welcome to the YelpCamp Family");
   }
 
   async sendPasswordReset() {
     await this.send(
-      'passwordReset',
-      'Your password reset token (valid for only 10 minutes)'
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
     );
   }
 };
